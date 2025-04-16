d6ac33c53adc47ee82bacb340d174c03e0b1203a
Francisco Colombo <franciscocolombounlp@gmail.com>
2025-04-16 10:54:59 -0300

    Se modifico la carpeta src y  se agrega las funciones

--- src/porcentaje_propietarios.py
+++
@@ -1,9 +0,0 @@
-from src.cant_casas import cant_casas
-
-def porcentaje_propietarios(datos):
-    cant_prop = 0
-    for renglon in datos:
-        if renglon[37] == '1':
-            cant_prop += int(renglon[8])
-    casas = cant_casas(datos)
-    return (cant_prop / casas) * 100
