dbfbc6b6107ae72a9c21fbe47d8aa40fc94cafc3
Tomas Jaureguiberry <jaureguiberry.tomas@gmail.com>
2025-04-16 13:43:44 -0300

    arreglo la ruta

--- src/funciones/crear_archivo_base.py
+++ src/funciones/crear_archivo_base.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def crear_individuo(archivo_individuos):
     """Recibe ubicacion donde crear el archivo maestro de individuos y el Path del detalle para agregar el header"""
     
-    detalle_i = Path("archivos") / "usu_individualT324.txt"
+    detalle_i = Path("archivos") / "usu_individual_T324.txt"
     archivo_i = open(archivo_individuos, 'w')
     with open(detalle_i) as archivo:
         csv_reader = csv.reader(archivo)
@@ -18,7 +18,7 @@ def crear_individuo(archivo_individuos):
 
 def crear_hogar(archivo_hogares):
     """Recibe ubicacion donde crear el archivo maestro de hogares y el Path del detalle para agregar el header"""
-    detalle_h = Path("archivos") / "usu_hogarT324.txt"
+    detalle_h = Path("archivos") / "usu_hogar_T324.txt"
     archivo_h = open(archivo_hogares, "w")
     with open(detalle_h) as archivo:
         csv_reader = csv.reader(archivo)
