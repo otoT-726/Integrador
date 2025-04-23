96d4ebff78ba76f08b0fba0b364ee98e3cb4fe9f
German Rodriguez <gerdepo04mail.com>
2025-04-23 10:56:11 -0300

    agrega CH04str (Inciso 3)

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from pathlib import Path
 from src.utils.agregar_archivo import agregar_trimestre
 from src.utils.crear_archivo_base import crear_hogar, crear_individuo
 from src.utils.rutas import project_path, data_path
+from src.utils.AgregarColumnaNombres import addColumna
+from src.utils.AgregarColumnaNivelED import addNivelED
 
 #Path configurable de detalle para agregar archivos al maestro
 
 detalle_i = data_path / "usu_individual_T24.txt"
-detalle_h = data_path / "usu_hogar_T324.txt"
+detalle_h = data_path / "usu_hogar_T24.txt"
 
 #Path maestros
 archivo_individuos = data_path / "archivo_individuos.txt"
@@ -21,6 +23,10 @@ archivo_hogares = data_path / "archivo_hogares.txt"
 """crear_hogar(archivo_hogares)"""
 
 try:
-    agregar_trimestre(archivo_hogares, detalle_h)
+    agregar_trimestre(archivo_individuos, detalle_i)
 except FileNotFoundError:
     print('El archivo que estás intentando agregar no existe o no se encuentra en la carpeta correspondiente')
+
+
+# Seccion a:
+#addColumna(archivo_individuos) punto 3
