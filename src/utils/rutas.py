8e076154dff0a055434f3a7e4617666909bce6b1
Tomas Jaureguiberry <jaureguiberry.tomas@gmail.com>
2025-04-23 09:42:42 -0300

    rutas importantes

--- src/utils/rutas.py
+++ src/utils/rutas.py
@@ -1,10 +1,5 @@
 from pathlib import Path
 
-
-
 project_path = Path(__file__).parent.parent.parent #Path a la carpeta global del trabajo
 
 data_path = project_path / "archivos" #Path a la carpeta de archivos
-
-
-print(data_path)
