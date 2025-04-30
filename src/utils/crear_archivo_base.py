49a26c8431da21403c2251f01656e6473335a43a
Tomas Jaureguiberry <jaureguiberry.tomas@gmail.com>
2025-04-30 11:51:52 -0300

    Agregamos funciones agregar trimestre completo

--- src/utils/crear_archivo_base.py
+++ src/utils/crear_archivo_base.py
@@ -1,14 +1,18 @@
 import csv
-from pathlib import Path
+from src.utils.rutas import data_path
 
 def crear_individuo(archivo_individuos):
     """Recibe ubicacion donde crear el archivo maestro de individuos y el Path del detalle para agregar el header"""
     
-    detalle_i = Path("archivos") / "usu_individual_T324.txt"
+    detalle_i = data_path / "usu_individual_T324.txt"
     
     with open(detalle_i) as archivo:
         csv_reader = csv.reader(archivo, delimiter=";")
         header = next(csv_reader)
+    header.append("CH04str")
+    header.append("NIVEL_ED_str")
+    header.append("CONDICION_LABORAL")
+    header.append("UNIVERSITARIO")
     with open(archivo_individuos, "w", newline="") as archivo:
         csv_writer = csv.writer(archivo, delimiter=";")
         csv_writer.writerow(header)
@@ -17,10 +21,14 @@ def crear_individuo(archivo_individuos):
 
 def crear_hogar(archivo_hogares):
     """Recibe ubicacion donde crear el archivo maestro de hogares y el Path del detalle para agregar el header"""
-    detalle_h = Path("archivos") / "usu_hogar_T324.txt"
-    archivo_h = open(archivo_hogares, "w")
+    detalle_h = data_path / "usu_hogar_T324.txt"
     with open(detalle_h) as archivo:
         csv_reader = csv.reader(archivo, delimiter=";")
         header= next(csv_reader)
+    header.append("TIPO_HOGAR")
+    header.append("MATERIAL_TECHUMBRE")
+    header.append("DENSIDAD_HOGAR")
+    header.append("CONDICION_DE_HABITABILIDAD")
     with open(archivo_hogares, "w", newline="") as archivo:
-        csv.writer(archivo, delimiter=";").writerow(header)
+        csv_writer = csv.writer(archivo, delimiter=";")
+        csv_writer.writerow(header)
