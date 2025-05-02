823a29de442310eaed8b06c910cef24e8dc1fd7b
Francisco Colombo <franciscocolombounlp@gmail.com>
2025-05-02 18:13:33 -0300

    Agrego el archivo LICENSE vacio y agrego algunas cosas en pages

--- Inicio.py
+++ Inicio.py
@@ -5,8 +5,15 @@ import streamlit as st
 #decir, que contiene la EPH.
 #En la etapa 2 se agrega las indicaciones de uso de la interfaz.
 
-st.title("la bandeja")
+#CONFIGURACIÓN DE LA PÁGINA
 
+st.set_page_config(page_title="Encuest App", page_icon=":rocket:", layout="wide")#CONFIGURACION DEL TITULO EN PESTAÑA Y EL ICONO
 
-st.subheader("")
+# FRONTEND
+
+st.title("Encuest App")
+
+st.divider()
+
+st.subheader("") #PARRAFO EXPLICATIVO DE LA EPH
 
---
+++ pages/01_Carga_de_datos.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#CONFIGURACIÓN DE LA PÁGINA
+
+st.set_page_config(page_title="Carga de datos", page_icon=":rocket:", layout="wide")
+
+# FRONTEND
+
+st.title('Carga de datos')
