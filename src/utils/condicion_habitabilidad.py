cedfc0b1efd8aa5cd8d5ec6066800b1b6f2ce4a6
Tomas Jaureguiberry <jaureguiberry.tomas@gmail.com>
2025-05-02 16:46:57 -0300

    actualizo inciso 10(agregar condicion de habitabilidad) a DictReader para evitar errores de indices.

--- src/utils/condicion_habitabilidad.py
+++ src/utils/condicion_habitabilidad.py
@@ -3,31 +3,33 @@ import csv
 def condicion_de_habitabilidad(ruta) -> None:           #Especifico que la funcion retorna None. Al ejecutarla solo actualiza al archivo
     """
     Recibe el archivo maestro y lo actualiza. Retorna None
-    Crea un sistema de puntajes basandose en la calidad del hogar
-    y depende el puntaje obtenido, la residencia se considera
+    Crea un sistema basandose en la calidad del hogar
+    y depende las condiciones cumplidas, la residencia se considera
     insuficiente, regular, saludable o buena.
     """
     with open(ruta) as archivo:
-        csv_reader = csv.reader(archivo, delimiter=";")
-        header, datos = next(csv_reader), list(csv_reader)
+        csv_reader = csv.DictReader(archivo, delimiter=";")
+        header, datos = csv_reader.fieldnames + ["CONDICION_DE_HABITABILIDAD"], list(csv_reader)
 
     with open(ruta, "w", newline="", encoding="utf-8") as archivo:
-        csv_writer = csv.writer(archivo, delimiter=";")
-        header.append("CONDICION_DE_HABITABILIDAD")
-        csv_writer.writerow(header)
+        csv_writer = csv.DictWriter(archivo, fieldnames= header, delimiter=";")
+        csv_writer.writeheader
+        
         for linea in datos:
-            tipo_piso = str(linea[12])
-            tiene_agua = str(linea[16])
-            tipo_agua = str(linea[17])
-            tiene_baño = str(linea[19])
-            donde_baño = str(linea[20])
-            tipo_baño = str(linea[21])
-            if(tiene_agua != "1" or tiene_baño != "1" or tipo_piso == "3" or donde_baño == "3"):
-                linea.append("Insuficiente")
-            elif(tipo_agua == "1" and tipo_baño == "1" and donde_baño == "1" and tipo_piso == "1"):
-                linea.append("Buena")
-            elif(tipo_agua != "3"  and donde_baño == "1" and tipo_baño != "3"): 
-                linea.append("Saludable")
+            tipo_piso = str(linea["IV3"])
+            tiene_agua = str(linea["IV6"])
+            tipo_agua = str(linea["IV7"])
+            tiene_baño = str(linea["IV8"])
+            donde_baño = str(linea["IV9"])
+            tipo_baño = str(linea["IV10"])
+            if(tiene_agua != "1" or tiene_baño != "1" or tipo_piso == "3" or donde_baño == "3"):        #Si no tiene agua, baño, pisos fijos o baño en su terreno se considera insuficiente
+                linea["CONDICION_DE_HABITABILIDAD"] = "Insuficiente"
+            elif(tipo_agua == "1" and tipo_baño == "1" and donde_baño == "1" and tipo_piso == "1"):     #Si tiene todas las necesidades basicas cubiertas se considera Buena
+                linea["CONDICION_DE_HABITABILIDAD"] = "Buena"
+            elif(tipo_agua != "3"  and donde_baño == "1" and tipo_baño != "3"):                         #Si tiene agua y un baño dentro de su casa se considera saludable
+                linea["CONDICION_DE_HABITABILIDAD"] = "Saludable"
             else:
-                linea.append("Regular")
+                linea["CONDICION_DE_HABITABILIDAD"] = "Regular"                                         #Todas las residencias que no hayan entrado en las variables anteriores,
+                                                                                                        # es decir que pueden contar con variedad de condiciones pero no extremas,
+                                                                                                        # se las considera regulares
             csv_writer.writerow(linea)
