bf24d0e9a08ec8caa54c8c47f2c589f8caeafa1f
Tomas Jaureguiberry <jaureguiberry.tomas@gmail.com>
2025-05-08 14:21:51 -0300

    Agregar genero sin modificar la estructura original del trimestre

--- src/utils/AgregarColumnaNombres.py
+++ src/utils/AgregarColumnaNombres.py
@@ -1,19 +1,21 @@
+from src.utils.rutas import data_path
 import csv
 def addColumna(archivo_individuos):
-    index_genero = 11
+    index_genero = "CH04"
     #traigo el archivo maestro
     with open(archivo_individuos, "r",newline='') as mae:
-        csv_reader = csv.reader(mae, delimiter=";")
-        header, cuerpo = next(csv_reader) ,list(csv_reader) 
+        csv_reader = csv.DictReader(mae, delimiter=";")
+        header = csv_reader.fieldnames + ["CH04str"]
+        filas = []
 
-    with open(archivo_individuos, "w",newline='') as mae:
-        csv_writer = csv.writer(mae,delimiter=";") # writter para indicar que voy a escribir el archivo
-        header.append("CH04str")                                        
-        csv_writer.writerow(header)     # es para escribir una linea, en este caso para el header                          
-        for dat in cuerpo:
+        for dat in csv_reader:
             if dat[index_genero] == "1":
-                genero_str = "Masculino"
+                dat["CH04str"] = "Masculino"
             else :
-                genero_str = "Femenino"
-            dat.append(genero_str) # dat es la linea,que es una lista, entonces a esa linea le agrego el genero y quedara modificada
-            csv_writer.writerow(dat) # con el writerow voy a escribir la linea dat 
+                dat["CH04str"] = "Femenino"
+            
+            filas.append(dat) # dat es la linea,que es una lista, entonces a esa linea le agrego el genero y quedara modificada
+
+    with open(data_path/"archivo_individuos.txt", "a",newline='') as mae:
+        csv_writer = csv.DictWriter(mae,delimiter=";", fieldnames=header) # writter para indicar que voy a escribir el archivo
+        csv_writer.writerows(filas) # con el writerow voy a escribir la linea dat
