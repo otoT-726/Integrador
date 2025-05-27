3020718cb06d06e1033ef7ff7f04b836cb372f6b
Tomas Jaureguiberry <jaureguiberry.tomas@gmail.com>
2025-05-27 12:23:29 -0300

    Resuelvo problemas a la hora de cargar los datos.

--- pages/01_Carga_de_datos.py
+++ pages/01_Carga_de_datos.py
@@ -1,5 +1,8 @@
 import csv
 import streamlit as st
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1] / 'src'))
 from src.utils.seccion_b.primer_ultimo_trim_ano import primerUltimoTrimAno
 from utils.seccion_a.agregar_archivo import agregar_trimestre_completo_hogar, agregar_trimestre_completo_individuo
 from utils.rutas import data_path
@@ -36,33 +39,35 @@ st.divider()
 #vamos a crear una lista con los archivos que ya tenemos en la base de datos
 
 
-def agregar(data_path):
-    listaArchivos = correspondencias()
+def agregar(data_path, lista_archivos):
+    lista_correspondencias = correspondencias()
     for archivo in data_path.iterdir(): # vamos a recorrer los archivos que hay en la carpeta de datos
         #for elem in listaArchivos:
-        if archivo.name not in listaArchivos:
+        if archivo.name == "ruta_nombres.txt":
+            continue
+        if archivo.name not in lista_archivos and archivo.name not in lista_correspondencias:
             
             with open(ruta_nombres, "w") as nombres:
-                csv.writer(nombres, delimiter=";").writerow(listaArchivos)
+                csv.writer(nombres, delimiter=";").writerow(lista_archivos)
 
-            if "hogar" in archivo.name.lower():
+            if "usu_hogar" in archivo.name.lower():
                 agregar_trimestre_completo_hogar(archivo)
                 st.success(f"Se ha agregado el archivo {archivo.name} a la base de datos de hogares")
                 #se agrego a la lista de nuestros archivos ya cargados,uno nuevo                    
-            elif "indi" in archivo.name.lower() :
+            elif "usu_indi" in archivo.name.lower() :
                 agregar_trimestre_completo_individuo(archivo)
                 st.success(f"Se ha agregado el archivo {archivo.name} a la base de datos de individuos")
         else:
             #Falta hacer la funcion para detallar de forma mas prolija el faltante 
-            st.warning('El archivo ', archivo.name, ' no tiene correspondencia.')
+            st.warning(f'El archivo {archivo.name} no tiene correspondencia.')
 
 ruta_nombres = data_path / "ruta_nombres.txt"
 with open(ruta_nombres) as file:
-    listaArchivos = list(csv.reader(file, delimiter= ";"))[0]
+    lista_archivos = list(csv.reader(file, delimiter= ";"))[0]
     with st.expander("Archivos en la base de datos"):
-        for nombre in listaArchivos:
+        for nombre in lista_archivos:
             st.write(nombre)
     if st.button("Agregar archivos nuevos"):
-        agregar(listaArchivos,data_path)
+        agregar(data_path,lista_archivos)
         st.success("Archivos agregados correctamente")
 
