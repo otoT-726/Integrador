31650d1c5708a29977d46d894e216317abc42261
German Rodriguez <gerdepo04mail.com>
2025-05-28 13:09:55 -0300

    se modifico caracteristicas demograficas

--- pages/02_Caracteristicas_demograficas.py
+++ pages/02_Caracteristicas_demograficas.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1] / 'src'))
 
-from src.utils.parte_2.funciones_p3.grafico_de_barras import generar_barras
+from src.utils.parte_2.P3.grafico_de_barras import generar_barras
 boton = st.button("Ingresar anio y luego trimestre")
 if boton:
     generar_barras()
