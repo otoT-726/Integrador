adabac951dbef5633c305da48c2767715d622e81
German Rodriguez <gerdepo04mail.com>
2025-05-28 10:10:21 -0300

    caracteristicas demograficas

---
+++ pages/02_Caracteristicas_demograficas.py
@@ -0,0 +1,10 @@
+#Pagina de ger 
+import streamlit as st
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1] / 'src'))
+
+from src.utils.parte_2.funciones_p3.grafico_de_barras import generar_barras
+boton = st.button("Ingresar anio y luego trimestre")
+if boton:
+    generar_barras()
