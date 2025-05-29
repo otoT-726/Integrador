a8f9bfbc59bf2af506aa6fa5e1865a554388e136
Manuel Mango <manuelmangoinfo@gmail.com>
2025-05-29 11:30:35 -0300

    Subida pagina

--- pages/06_Ingresos.py
+++ pages/06_Ingresos.py
@@ -1,6 +1,6 @@
 from src.utils.parte_2.P7.ingresos import debajo_de_lineas
 import streamlit as st
-def main():
+def main(): 
     st.title("Análisis de Ingresos y Pobreza")
     debajo_de_lineas()
 if __name__ == "__main__":
