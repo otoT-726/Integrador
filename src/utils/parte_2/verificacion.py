57b877e774d0ea557f7534cdb1469cd871345bd7
Francisco Colombo <franciscocolombounlp@gmail.com>
2025-05-29 11:04:21 -0300

    Agrego año 2023 completo y verficacion version 2

--- src/utils/parte_2/verificacion.py
+++ src/utils/parte_2/verificacion.py
@@ -1,4 +1,5 @@
 from utils.rutas import data_path
+
 def correspondencias():
     """Funcion que corrobora las correspondencias de los archivos individuos y hogares"""
     listaArchivosActuales = []
@@ -10,26 +11,28 @@ def correspondencias():
     
     dicContador = {}
     for archivo in listaArchivosActuales:
-        
-        #Necesito agarrar los codigos de los archivos
-        if(archivo.startswith('usu')):
-            codigo = archivo.split("_")[2].split(".")[0]
-                    
-            if(codigo not in dicContador):
-                dicContador[codigo] = 1
-            else:
-                dicContador[codigo] += 1
-            
+        if archivo.startswith('usu'):
+            partes = archivo.split("_")
+            tipo = partes[1]  # 'hogar' o 'individual'
+            codigo = partes[2].split(".")[0]
+
+            if codigo not in dicContador:
+                dicContador[codigo] = {"hogar": 0, "individual": 0}
+
+            if "hogar" in tipo:
+                dicContador[codigo]["hogar"] += 1
+            elif "individual" in tipo:
+                dicContador[codigo]["individual"] += 1
+
     listaResultante = []
-    
-    for clave,valor in dicContador.items():
-        if(valor == 1):
+    for clave, valor in dicContador.items():
+        if valor["hogar"] == 0 or valor["individual"] == 0:
             listaResultante.append(clave)
 
     listaNombres = []
     for codigo in listaResultante:
         for archivo in listaArchivosActuales:
-            if(codigo in archivo):
+            if codigo in archivo:
                 listaNombres.append(archivo)
     
     return listaNombres
