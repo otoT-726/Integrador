7e3b0b658759fb66993f476bf508a9c3c8da2554
Tomas Jaureguiberry <jaureguiberry.tomas@gmail.com>
2025-05-30 19:43:27 -0300

    Avances en apartado Caracteristicas de las viviendas

--- pages/03_Caracteristicas_de_la_vivienda.py
+++ pages/03_Caracteristicas_de_la_vivienda.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from src.utils.parte_2.P4.porcentaje_pisos import retornar_informacion_pisos
 from src.utils.parte_2.P4.porcentaje_baños import retornar_informacion_baños
 from src.utils import rutas
-
+from src.utils import diccionario_aglomerados
 
 sys.path.append(str(Path(__file__).parent.parent / 'src'))
 
@@ -43,7 +43,7 @@ with tab2:
     #Esta linea la uso por pura estetica. No estoy seguro si es la mejor manera de hacerlo
     info = "" if año == None else "en el año " + str(año)
     
-    st.header("Cantidad total de viviendas analizadas" + info + ": ")
+    st.header("Cantidad total de viviendas analizadas " + info + ": ")
     st.header(str(archivo_hogares["PONDERA"].sum()))
     st.divider()
     
@@ -65,7 +65,7 @@ with tab2:
     
     #Creo el gráfico
     fig, ax = plt.subplots()
-    ax.pie(conteo_tipos, labels=index_list, autopct='%1.1f%%')
+    ax.pie(conteo_tipos, labels=index_list, autopct='%1.1f%%', colors=["#803892", "#2BB39C", "#4087A3"])
     ax.set_title('Proporción de viviendas por tipo')
     ax.axis('equal')
     
@@ -109,4 +109,98 @@ with tab2:
     resultado = retornar_informacion_baños(archivo_hogares)
     st.write(resultado)
 
+    st.divider()
+
+    #Punto 1.4.5: Además del año seleccionado, se debe ingresar un aglomerado específico. Se mostrará
+                # la evolución del régimen de tenencia (propia, alquilada, cedida, etc.) para ese aglomerado.
+                # El usuario debe poder elegir el tipo de tenencia que desea ver: una, un conjunto o todas.
+
+    st.subheader("EVOLUCION DEL REGIMEN DE TENENCIA")
+
+    st.write("A continuacion podra elegir un aglomerado para visualizar la evolución del regimen de tenencia de los habitantes sobre sus viviendas.")
+
+
+    conteo_aglomerados = archivo_hogares["AGLOMERADO"].unique()
     
+
+    #Guardo los nombres de los aglomerados en una lista, para luego permitir seleccionar el aglomerado por nombre.
+    nombres_aglomerados = []
+    for valor in diccionario_aglomerados.values():
+        nombres_aglomerados.append(valor)
+
+    aglomerado_incluido = st.selectbox("Elija que aglomerado desea visualizar:", nombres_aglomerados, index= None, placeholder="SELECCIONE UN AGLOMERADO")
+
+    #aglomerados_incluidos = st.multiselect("Elija los aglomerados que desea visualizar:", nombres_aglomerados, placeholder="TODOS") -> Para poder elegir multiples aglomerados(INCOMPLETO)
+
+    #Creo la lista para elegir el regimen de tenencia por nombre
+    lista_tenencia = ["Propietario de la vivienda y el terreno", "Propietario únicamente de la vivienda",
+                    "Inquilino", "Ocupante por pago de impuestos", "Ocupante en relacion de dependencia",
+                    "Ocupante gratuito", "Ocupante de hecho", "En sucesión"]
+
+    #Filtro los tipos de tenencia en dependencia de lo que seleccione el usuario.
+    tenencia_incluida = st.multiselect("Elija el régimen de tenencia que desea visualizar:", lista_tenencia, placeholder="TODOS")
+    if not tenencia_incluida:
+        tenencia_incluida = lista_tenencia
+
+
+    #Espero a que ingresen un trimestre para continuar.
+    if(aglomerado_incluido == None):
+        st.write("Seleccione un aglomerado para visualizar su información")
+    else:
+        st.subheader(aglomerado_incluido)
+
+        #Creo un diccionario invertido de aglomerados para poder acceder al codigo mediante el nombre. Y creo un diccionario para acceder a los codigos del regimen de vivienda('II7') y viceversa.
+
+        diccionario_aglomerados_invertido = {valor: clave for clave, valor in diccionario_aglomerados.items()}
+
+        diccionario_regimen = {lista_tenencia[i]: i+1 for i in range(0, len(lista_tenencia))}
+
+        diccionario_regimen_invertido = {valor: clave for clave, valor in diccionario_regimen.items()}
+
+        cod_aglomerado = diccionario_aglomerados_invertido[aglomerado_incluido]
+
+        #Uso list-comprehension para recuperar los codigos de tenencia que se incluyen a la visualización. Nótese que tenencia_incluida es una lista y por eso no se puede usar como clave.
+        codigos_tenencia = [diccionario_regimen[tenencia] for tenencia in tenencia_incluida]
+        mask = (archivo_hogares["AGLOMERADO"] == cod_aglomerado) & (archivo_hogares["II7"].isin(codigos_tenencia))
+        
+        #Filtro la ponderación de hogares por aglomerado y regimenes seleccionados. Realizo una copia porque voy a agregar mas columnas a este dataframe para graficar.
+        filtro = archivo_hogares[mask].copy()
+        
+        #Agrupo por año, trimestre y regimen seleccionado
+        grupo = filtro.groupby(["ANO4", "TRIMESTRE", "II7"])["PONDERA"].sum().reset_index()
+        
+        #Agrego columna 'PERIODO' para agrupar año y trimestre, y columna 'TENENCIA' para recuperar el nombre del regimen basado en su codigo.
+        grupo["PERIODO"] = grupo["ANO4"].astype(str) + "T" + grupo["TRIMESTRE"].astype(str)
+
+        
+        grupo["TENENCIA"] = grupo["II7"].map(diccionario_regimen_invertido)
+
+        #Grafico.
+        fig, ax = plt.subplots(figsize=(12, 9))
+        #Itero por cada uno de los tipos de tenencia y grafico en función de si se encuentran seleccionadas. Ademas voy guardando las etiquetas para luego mostrar cada una por separado.
+        for tenencia in grupo["TENENCIA"].unique():
+            data = grupo[grupo["TENENCIA"] == tenencia]
+            ax.plot(data["PERIODO"], data["PONDERA"], marker='o', label=tenencia)
+
+        ax.set_title(f"EVOLUCION REGIMEN DE TENENCIA")
+        ax.set_xlabel("Periodo (Año-Trimestre)")
+        ax.set_ylabel("Cantidad de viviendas segun su tipo")
+        ax.legend()
+        plt.grid()
+        plt.xticks(rotation=45)
+
+        st.pyplot(fig)
+
+
+    st.divider()
+
+    #Punto 1.4.6:  Informar de manera ordenada (decreciente) la cantidad de viviendas ubicadas en villa 
+                # de emergencia por aglomerado. Además de la cantidad informar el porcentaje con respecto al total.
+
+    #Columna 'IV12_3' define si la vivienda se ubica en una villa de emergencia. 1=si. 2=no
+
+    st.subheader("CANTIDAD DE VIVIENDAS UBICADAS EN VILLAS EMERGENCIA POR AGLOMERADO")
+
+
+
+    mask = 
