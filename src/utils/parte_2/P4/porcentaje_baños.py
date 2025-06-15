9fb2d4674011e6ad1f0721abf1307e664883b5c6
Tomas Jaureguiberry <jaureguiberry.tomas@gmail.com>
2025-06-14 21:31:43 -0300

    Porcentaje de casas con baño por aglomerado

--- src/utils/parte_2/P4/porcentaje_baños.py
+++ src/utils/parte_2/P4/porcentaje_baños.py
@@ -1,19 +1,19 @@
 import pandas as pd
 from src.utils import diccionario_aglomerados
 
-def calcular_porcentajes(data):
+def calcular_porcentajes(data: pd.DataFrame):
     """Esta función suplementaria para el punto 1.4.4 devuelve una Serie que contiene el aglomerado,
     el porcentaje de viviendas que tienen baños interiores, y porcentaje de las viviendas que no"""
 
     total = data['PONDERA'].sum()
-    primer_material = data.loc[data['IV8'] == 1, 'PONDERA'].sum() / total * 100
-    segundo_material = data.loc[data['IV8'] == 2, 'PONDERA'].sum() / total * 100
+    con = data.loc[data['IV8'] == 1, 'PONDERA'].sum() / total * 100
+    sin = data.loc[data['IV8'] == 2, 'PONDERA'].sum() / total * 100
     return pd.Series({
-        '% Con baños interiores': round(primer_material, 2),
-        '% Sin baños interiores': round(segundo_material, 2),
+        '% Con baños interiores': round(con, 2),
+        '% Sin baños interiores': round(sin, 2),
     })
 
-def retornar_informacion_baños(data):
+def retornar_informacion_baños(data: pd.DataFrame):
     """Retorna el dataframe con el porcentaje de tenencia de baños por aglomerado."""
     
     #Toma todos los que tienen baño (1) y los que no (2)
